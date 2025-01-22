61d5063916613599b657d35d4b61468ff996c6ff
Thibault <tibus.gic@gmail.com>
2025-01-22 19:30:30 +0100

    fix config.php

--- public_html/include/config.php
+++ public_html/include/config.php
@@ -2,16 +2,17 @@
 require_once('/home/vendor/autoload.php');
 require_once('/home/GoogleAuthenticator.php');
 
-session_start();
+// Démarrer la session si nécessaire
+if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
 
 $db = new PDO('mysql:host=localhost;dbname=thibaultgicquel6201;charset=utf8mb4', 'thibaultgicquel6201', 'WOWjSaNlCy8C');
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader, array(
         'cache' => false
 ));
 
 date_default_timezone_set('Europe/Paris');
-
-
 
